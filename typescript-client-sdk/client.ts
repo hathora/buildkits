f9c87f1b8db46daf4914359559b2317d88636604
Austin Buckler <austin@lionhat.co>
2022-12-27 23:39:44 -0700

    added loginSiwe to typescript-sdk

--- typescript-client-sdk/client.ts
+++ typescript-client-sdk/client.ts
@@ -1,33 +1,64 @@
 import axios from "axios";
 import jwtDecode from "jwt-decode";
-import { HathoraTransport, TCPHathoraTransport, TransportType, WebSocketHathoraTransport } from "./transport.js";
+import {
+  HathoraTransport,
+  TCPHathoraTransport,
+  TransportType,
+  WebSocketHathoraTransport,
+} from "./transport.js";
 
 export class HathoraClient {
   public static getUserFromToken(token: string): object & { id: string } {
     return jwtDecode(token);
   }
 
-  public constructor(private appId: string, private coordinatorHost: string = "coordinator.hathora.dev") {}
+  public constructor(
+    private appId: string,
+    private coordinatorHost: string = "coordinator.hathora.dev"
+  ) {}
 
   public async loginAnonymous(): Promise<string> {
-    const res = await axios.post(`https://${this.coordinatorHost}/${this.appId}/login/anonymous`);
+    const res = await axios.post(
+      `https://${this.coordinatorHost}/${this.appId}/login/anonymous`
+    );
     return res.data.token;
   }
 
   public async loginNickname(nickname: string): Promise<string> {
-    const res = await axios.post(`https://${this.coordinatorHost}/${this.appId}/login/nickname`, { nickname });
+    const res = await axios.post(
+      `https://${this.coordinatorHost}/${this.appId}/login/nickname`,
+      { nickname }
+    );
     return res.data.token;
   }
 
   public async loginGoogle(idToken: string): Promise<string> {
-    const res = await axios.post(`https://${this.coordinatorHost}/${this.appId}/login/google`, { idToken });
+    const res = await axios.post(
+      `https://${this.coordinatorHost}/${this.appId}/login/google`,
+      { idToken }
+    );
+    return res.data.token;
+  }
+
+  public async loginSiwe(message: string, signature: string): Promise<string> {
+    const res = await axios.post(`http://localhost:3000/verify`, {
+      message,
+      signature,
+    });
     return res.data.token;
   }
 
   public async create(token: string, data: ArrayBuffer): Promise<string> {
-    const res = await axios.post(`https://${this.coordinatorHost}/${this.appId}/create`, data, {
-      headers: { Authorization: token, "Content-Type": "application/octet-stream" },
-    });
+    const res = await axios.post(
+      `https://${this.coordinatorHost}/${this.appId}/create`,
+      data,
+      {
+        headers: {
+          Authorization: token,
+          "Content-Type": "application/octet-stream",
+        },
+      }
+    );
     return res.data.stateId;
   }
 
@@ -43,7 +74,9 @@ export class HathoraClient {
     return connection;
   }
 
-  private getConnectionForTransportType(transportType: TransportType): HathoraTransport {
+  private getConnectionForTransportType(
+    transportType: TransportType
+  ): HathoraTransport {
     if (transportType === TransportType.WebSocket) {
       return new WebSocketHathoraTransport(this.appId, this.coordinatorHost);
     } else if (transportType === TransportType.TCP) {
