1434b356d0087a86a6ff351900562fe0a94db584
Harsh Pandey <hpandey793@gmail.com>
2023-01-26 23:08:32 -0500

    axios -> fetch

--- typescript-client-sdk/client.ts
+++ typescript-client-sdk/client.ts
@@ -1,4 +1,3 @@
-import axios from "axios";
 import jwtDecode from "jwt-decode";
 import { ConnectionInfo, HathoraConnection } from "./connection";
 
@@ -10,39 +9,48 @@ export class HathoraClient {
   public constructor(private appId: string, private defaultConnectionInfo: ConnectionInfo) {}
 
   public async loginAnonymous(): Promise<string> {
-    const res = await axios.post(`https://hathora-api.fly.dev/v2/auth/${this.appId}/login/anonymous`);
-    return res.data.token;
+    const res = await this.postJson(`https://hathora-api.fly.dev/v2/auth/${this.appId}/login/anonymous`, {});
+    return res.token;
   }
 
   public async loginNickname(nickname: string): Promise<string> {
-    const res = await axios.post(`https://hathora-api.fly.dev/v2/auth/${this.appId}/login/nickname`, { nickname });
-    return res.data.token;
+    const res = await this.postJson(`https://hathora-api.fly.dev/v2/auth/${this.appId}/login/nickname`, { nickname });
+    return res.token;
   }
 
   public async loginGoogle(idToken: string): Promise<string> {
-    const res = await axios.post(`https://hathora-api.fly.dev/v2/auth/${this.appId}/login/google`, { idToken });
-    return res.data.token;
+    const res = await this.postJson(`https://hathora-api.fly.dev/v2/auth/${this.appId}/login/google`, { idToken });
+    return res.token;
   }
 
   public async createPrivateLobby(token: string): Promise<string> {
-    const res = await axios.post(
+    return await this.postJson(
       `https://hathora-api.fly.dev/v2/lobby/${this.appId}/create/unlisted`,
       {},
-      { headers: { Authorization: token } }
+      { Authorization: token }
     );
-    return res.data;
   }
 
   public async getConnectionInfoForRoomId(roomId: string, tls: boolean = true): Promise<ConnectionInfo> {
-    const res = await axios.get(`https://hathora-api.fly.dev/v2/rooms/${this.appId}/connectioninfo/${roomId}`);
-    if (res.data.host === "") {
+    const res = await fetch(`https://hathora-api.fly.dev/v2/rooms/${this.appId}/connectioninfo/${roomId}`);
+    const data = await res.json();
+    if (data.host === "") {
       return this.defaultConnectionInfo;
     }
-    return { host: res.data.host, port: res.data.port, tls };
+    return { host: data.host, port: data.port, tls };
   }
 
   public async newConnection(roomId: string, tls: boolean = true): Promise<HathoraConnection> {
     const connectionInfo = await this.getConnectionInfoForRoomId(roomId, tls);
     return new HathoraConnection(roomId, connectionInfo);
+  }
+
+  private async postJson(url: string, body: any, headers: Record<string, string> = {}) {
+    const res = await fetch(url, {
+      method: "POST",
+      headers: { ...headers, "Content-Type": "application/json" },
+      body: JSON.stringify(body),
+    });
+    return await res.json();
   }
 }
